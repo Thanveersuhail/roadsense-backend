8fe5322b0cd6263a21d32acad271988b83c925f4
Thanveersuhail T <thanveersuhail3115@gmail.com>
2026-04-23 18:44:24 +0530

    Add dj-database-url for Render PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project l
