016b93ce6c4dd616450359d0ee82755526d41eba
Thanveersuhail <thanveersuhail3115@gmail.com>
2026-05-03 01:49:25 +0530

    Make device_name optional in EventSerializer

--- events/serializers.py
+++ events/serializers.py
@@ -7,7 +7,7 @@ class DeviceSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'created_at']
 
 class EventSerializer(serializers.ModelSerializer):
-    device_name = serializers.CharField(write_only=True)
+    device_name = serializers.CharField(write_only=True, required=False)
     device = DeviceSerializer(read_only=True)
 
     class Meta:
@@ -22,7 +22,7 @@ class EventSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_at', 'device']
 
     def create(self, validated_data):
-        device_name = validated_data.pop('device_name')
+        device_name = validated_data.pop('device_name', 'unknown-device')
         device, created = Device.objects.get_or_create(name=device_name)
         event = Event.objects.create(device=device, **validated_data)
         return event
